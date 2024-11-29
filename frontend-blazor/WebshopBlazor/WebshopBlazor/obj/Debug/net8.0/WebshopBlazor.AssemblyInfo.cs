52802cd00661acc2c83e7cdc0e21e780773f1445
Ragee1999 <ragti100@gmail.com>
2024-11-29 13:10:38 +0100

    gitignore issue

--- frontend-blazor/WebshopBlazor/WebshopBlazor/obj/Debug/net8.0/WebshopBlazor.AssemblyInfo.cs
+++ frontend-blazor/WebshopBlazor/WebshopBlazor/obj/Debug/net8.0/WebshopBlazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebshopBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b8cf826a7eeaaf45bc9359ca6914ad1cb0a78a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+392e1a6a26f7bd63c92eca40f7a07e32e13632a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebshopBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebshopBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
