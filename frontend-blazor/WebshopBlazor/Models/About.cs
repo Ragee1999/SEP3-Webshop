99b3ac61e028ebb6710ad1eaaca4467da87a2449
zakaria2899 <61026115+zakaria2899@users.noreply.github.com>
2024-12-03 13:47:14 +0100

    Første del af frontenden

---
+++ frontend-blazor/WebshopBlazor/Models/About.cs
@@ -0,0 +1,8 @@
+﻿namespace WebshopBlazor.Models;
+
+public class AboutUs
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+}
---
+++ frontend-blazor/WebshopBlazor/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace WebshopBlazor.Models;
+
+public class Product
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public string ImageUrl { get; set; }
+}
