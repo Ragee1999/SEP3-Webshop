97d67409caa9fd3f19d97cfeb8dc37a0be1ad8dd
zakaria2899 <61026115+zakaria2899@users.noreply.github.com>
2024-12-11 20:50:38 +0100

    De rigtige beskrivelser og priser er indsat i databasen

--- backend-java/webshop/src/main/resources/data.sql
+++ backend-java/webshop/src/main/resources/data.sql
@@ -43,9 +43,36 @@ id SERIAL PRIMARY KEY,
 name VARCHAR(255) NOT NULL,
 email VARCHAR(255) NOT NULL,
 message TEXT NOT NULL,
-created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-is_answered boolean default false
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+ALTER TABLE product
+    ALTER COLUMN description TYPE character varying(1000);
+
+
+
+
+UPDATE product
+SET
+description = CASE
+WHEN id = 1 THEN 'A sleek and powerful laptop featuring an Intel Core i7 processor, 16GB RAM, and a 512GB SSD for lightning-fast performance. Perfect for work, study, and entertainment. Enjoy its stunning 15.6'' Full HD display and long-lasting battery life, all packed into a lightweight and stylish design.'
+WHEN id = 2 THEN 'Experience cutting-edge technology with this advanced smartphone, featuring a sleek design, high-resolution display, powerful performance, and a long-lasting battery to keep you connected all day.'
+WHEN id = 3 THEN 'Immerse yourself in crystal-clear sound with these noise-cancelling headphones, offering exceptional comfort, premium audio quality, and wireless connectivity for an unmatched listening experience.'
+WHEN id = 4 THEN 'Enhance your productivity and entertainment with this 4K UHD monitor, delivering stunning visuals, vibrant colors, and a sleek design perfect for work or play.'
+WHEN id = 5 THEN 'Elevate your typing and gaming experience with this RGB mechanical keyboard, featuring customizable lighting, responsive keys, and durable construction for long-lasting performance.'
+WHEN id = 6 THEN 'Stay productive and entertained on the go with this portable tablet, offering a high-resolution display, powerful performance, and a lightweight design for ultimate versatility.'
+WHEN id = 7 THEN 'Work and play with precision using this ergonomic wireless mouse, designed for comfort, smooth navigation, and reliable performance.'
+END,
+price = CASE
+WHEN id = 1 THEN 7999
+WHEN id = 2 THEN 8999
+WHEN id = 3 THEN 3200
+WHEN id = 4 THEN 2300
+WHEN id = 5 THEN 1400
+WHEN id = 6 THEN 3500
+WHEN id = 7 THEN 249
+END
+WHERE id IN (1, 2, 3, 4, 5, 6, 7);
 
 
 
